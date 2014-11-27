c876d2dc9b493321c19fbf2b4894bd329c19ba91
Sachin Wagh <sachin@amuratech.com>
2014-11-27 18:28:28 +0530

    Done CRUD of projects and developers.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProjectsController < ApplicationController
-  
+  before_filter :load_developer_new, :only => [:new]
+  before_filter :load_developer_create, :only => [:create]
   layout "admin"
   
   def index
@@ -9,9 +10,55 @@ class Admin::ProjectsController < ApplicationController
 
   def edit
     @project = Project.find(params[:id])
+    @developer = @project.developer
   end
 
-  def new
-    @project = Project.new
+  def update
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      if @project.update_attributes(params[:project])
+        flash[:success_message] = "Project updated successfully."
+        format.html { redirect_to admin_projects_path }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+
+  def new    
+    @project = @developer.projects.build
+  end
+
+  def create
+    @project = @developer.projects.build(params[:project])
+    respond_to do |format|
+      if @project.save
+        flash[:success_message] = "Project created successfully."
+        format.html { redirect_to admin_projects_path }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    respond_to do |format|
+      if @project.destroy
+        flash[:success_message] = "Project destroyed successfully."
+        format.html { redirect_to admin_projects_path }
+      else
+        
+      end
+    end
+  end
+
+  private
+  def load_developer_new
+    @developer = Developer.find(params[:developer])
+  end
+  def load_developer_create
+    @developer = Developer.find(params[:project][:developer_id])
   end
 end
